ca0e89c944570baf3936c04ab17f441e2d383a61
Msnbek <mahmutsenbek@icloud.com>
2022-10-21 23:04:10 +0300

    lastest version

--- Egg Timer App/ViewController.swift
+++ Egg Timer App/ViewController.swift
@@ -6,14 +6,70 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
-
+    @IBOutlet weak var timerView: UIView!
+    var timer = Timer()
+    let eggTimer = ["Soft": 3,
+                    "Medium": 5,
+                    "Hard": 7]
+    var passedTime = 0
+    var totalTime = 0
+    var player: AVAudioPlayer!
+   
+    
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var labelText: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+ 
+       
+        
     }
-
-
+   
+        
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        progressBar.progress = 0
+        labelText.text = hardness
+        totalTime = 0
+      //  print(eggTimer[hardness]!)
+        totalTime = eggTimer[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(zaman), userInfo: nil, repeats: true)
+        
+       
+       
+    }
+    @objc func zaman() {
+        if passedTime < totalTime {
+            passedTime += 1
+            let progressPercent = Float(passedTime) / Float(totalTime)
+            print(progressPercent)
+            progressBar.progress = progressPercent
+                print(passedTime)
+             //  passedTime = 0
+            
+        }else {
+           
+            timer.invalidate()
+            labelText.text = "Your egg is ready!"
+            print("total time")
+            playSound()
+            passedTime = 0
+        }
+    }
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    
 }
 
+}
